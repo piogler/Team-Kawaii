d64a755b286b21354857dfce8f314a3b396992db
bismelheenakhan <hbk_55@hotmail.com>
2016-03-15 23:07:50 -0400

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -1,17 +1,20 @@
 <?php
 /**
  * The sidebar containing the main widget area.
- *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
  * @package Team_Kawaii
+ *
  */
-
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-	return;
-}
+		
+if ( is_home () || is_page() || is_single() || is_archive ){ ?> 	
+				<div id="sidebar" class="widget-area side-widget-area"role="complementary">     	
+					<?php if(is_active_sidebar('the_sidebar')):?> 			
+						<div class="sidebar-widgets">             
+							<?php dynamic_sidebar('the_sidebar');?> 		
+						</div>        
+					 <?php endif; ?> 
+				</div> <?php } 
+	
 ?>
-
-<aside id="secondary" class="widget-area" role="complementary">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
-</aside><!-- #secondary -->
+	
