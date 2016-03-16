007bafcd4b629f4d8b5ad171c53ac0a97643bc94
bismelheenakhan <hbk_55@hotmail.com>
2016-03-16 01:29:48 -0400

    Adding template part to index

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+		<?php get_template_part( 'template-parts/front-page/about', 'none' ); ?> /* Adding the About Section */
 
 		<?php
 		if ( have_posts() ) :
