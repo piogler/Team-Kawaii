3450821c8dcce89349fddd44288e20d76ecafbc1
bismelheenakhan <hbk_55@hotmail.com>
2016-03-16 03:05:26 -0400

    Updated styling for posts

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -8,13 +8,13 @@
  */
 
 ?>
-
+<div class="
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
+	<header class="page-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
-	<div class="entry-content">
+	<div class="page-content">
 		<?php
 			the_content();
 
