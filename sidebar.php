e36b197d54c2d611f4c4efde1616c1354dda5ef4
piogler <petesadujko@gmail.com>
2016-03-16 02:47:40 -0400

    Revert "Update sidebar.php"
    This reverts commit d64a755b286b21354857dfce8f314a3b396992db.

--- sidebar.php
+++ sidebar.php
@@ -1,20 +1,17 @@
 <?php
 /**
  * The sidebar containing the main widget area.
+ *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
  * @package Team_Kawaii
- *
  */
-		
-if ( is_home () || is_page() || is_single() || is_archive ){ ?> 	
-				<div id="sidebar" class="widget-area side-widget-area"role="complementary">     	
-					<?php if(is_active_sidebar('the_sidebar')):?> 			
-						<div class="sidebar-widgets">             
-							<?php dynamic_sidebar('the_sidebar');?> 		
-						</div>        
-					 <?php endif; ?> 
-				</div> <?php } 
-	
+
+if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+	return;
+}
 ?>
-	
+
+<aside id="secondary" class="widget-area" role="complementary">
+	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+</aside><!-- #secondary -->
